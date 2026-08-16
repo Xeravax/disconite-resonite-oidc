0c32853d4b6f4d1a0c6706d9b07e39731de7e422
Stefano Haagmans <stefano@stefanocoding.me>
2026-08-17 00:49:49 +0200

    Refactor Resonite OAuth login method initializer for improved clarity
    - Removed the unused `withPluginApi` functionality and the associated `PLUGIN_ID` constant from the Resonite OAuth login method initializer.
    - Streamlined the code to focus solely on setting the OAuth login icon URL.
    
    These changes aim to enhance code readability and maintainability by eliminating unnecessary complexity.
    
    Signed-off-by: Stefano Haagmans <stefano@stefanocoding.me>

--- assets/javascripts/discourse/initializers/resonite-oauth-login-method.js
+++ assets/javascripts/discourse/initializers/resonite-oauth-login-method.js
@@ -1,7 +1,4 @@
-import { withPluginApi } from "discourse/lib/plugin-api";
 import getURL from "discourse/lib/get-url";
-
-const PLUGIN_ID = "disconite-resonite-oidc";
 
 export default {
   name: "resonite-oauth-login-method",
@@ -11,19 +8,5 @@ export default {
       "--resonite-oauth-login-icon-url",
       `url("${getURL("/plugins/disconite-resonite-oidc/resonite-login-icon.svg")}")`,
     );
-
-    withPluginApi("1.0.0", (api) => {
-      api.modifyClass("model:login-method", {
-        pluginId: PLUGIN_ID,
-
-        get icon() {
-          return (
-            this.icon_override ??
-            this.iconOverride ??
-            "user"
-          );
-        },
-      });
-    });
   },
 };
